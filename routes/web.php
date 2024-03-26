271443fe3a5b5030f26b96cab2b8e2d03319b128
NaufalParamaRafif <profesoraiueo@gmail.com>
2024-03-27 00:08:27 +0700

    Add Home Controller and Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
@@ -16,12 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home.home');
-});
-Route::get('/home', function () {
-    return view('home.home');
-});
+Route::get('/', [HomeController::class, 'show']);
+Route::get('/home', [HomeController::class, 'show']);
 
 Route::get('/ditandai', function () {
     return view('ditandai.ditandai');
