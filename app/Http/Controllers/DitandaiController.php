fa3bf274929c1d659394bbe8336df55bce400720
NaufalParamaRafif <profesoraiueo@gmail.com>
2024-03-29 20:41:20 +0700

    Edit DitandaiController (Fix Redirect)

--- app/Http/Controllers/DitandaiController.php
+++ app/Http/Controllers/DitandaiController.php
@@ -9,34 +9,21 @@ use Illuminate\Support\Facades\DB;
 
 class DitandaiController extends Controller
 {
-    public function show(User $user){
+    public function show(){
         $currentUser = auth()->user();
-        $user = User::find($user->id);
-        // validate current user == /ditandai/{{ user }}
-        if(!($currentUser == $user)){
-            return redirect('/');
-            die;
-        }
         return view('ditandai.ditandai', [
-            'bukus' => $user->buku_ditandai,
+            'bukus' => $currentUser->buku_ditandai,
         ]);
     }
 
     public function tandai(Buku $buku){
         $currentUser = auth()->user();
 
-        if($buku->is_ditandai())
+        if(!($buku->is_ditandai()))
         {
-            redirect('/ditandai/'.$currentUser->username);
-        }
-        else
-        {
-            // execute tandai
             User::find($currentUser->id)->buku_ditandai()->attach($buku);
         }
-        return view('ditandai.ditandai', [
-            'bukus' => $currentUser->buku_ditandai,
-        ]);
+        return redirect('/ditandai');
     }
 
     public function hapus_penanda(Buku $buku){
@@ -44,15 +31,8 @@ class DitandaiController extends Controller
 
         if($buku->is_ditandai())
         {
-            // execute destroy
             User::find($currentUser->id)->buku_ditandai()->detach($buku);
         }
-        else
-        {
-            redirect('/ditandai/'.$currentUser->username);
-        }
-        return view('ditandai.ditandai', [
-            'bukus' => $currentUser->buku_ditandai,
-        ]);
+        return redirect('/ditandai');
     }
 }
