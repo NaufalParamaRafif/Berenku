8d71d412aca88a93ef5aefca7ecea4e62d00adda
NaufalParamaRafif <profesoraiueo@gmail.com>
2024-04-01 22:27:54 +0700

    Add Modal for Pinjam and Kembalikan

--- resources/views/main/buku.blade.php
+++ resources/views/main/buku.blade.php
@@ -9,7 +9,19 @@
         <ul class="list-group list-group-flush">
             <li class="list-group-item">Penulis: {{ $buku->penulis()->get()[0]->name; }}</li>
             <li class="list-group-item">Kategori: {{ $buku->category }}</li>
-            <li class="list-group-item">{{ $buku->is_tersedia ? 'Tersedia' : 'Tidak tersedia' }}</li>
+            @guest
+                <li class="list-group-item">Status: {{ $buku->is_tersedia ? 'Tersedia' : 'Tidak tersedia' }}</li>
+            @endguest
+            @auth
+                @if ($buku->is_dipinjam())
+                    <li class="list-group-item">Status: Dipinjam</li>
+                @else
+                    <li class="list-group-item">Status: {{ $buku->is_tersedia ? 'Tersedia' : 'Tidak tersedia' }}</li>
+                @endif
+            @endauth
+            @if ($buku->deadline)
+                <li class="list-group-item">Akan dikembalikan pada: {{ $buku->deadline }}</li>
+            @endif
         </ul>
         <div class="card-body">
             @auth
@@ -19,9 +31,13 @@
                     <a href="/tandai/{{ $buku->slug }}" class="btn btn-danger">Tandai</a>
                 @endif
                 @if ($buku->is_dipinjam())
-                    <a href="/kembalikan/{{ $buku->slug }}" class="btn btn-success">Kembalikan</a>
+                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#kembalikanModal">
+                        Kembalikan
+                    </button>
                 @elseif ($buku->is_tersedia)
-                    <a class="btn btn-success" href="/pinjam/{{ $buku->slug }}">Pinjam</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pinjamModal">
+                        Pinjam
+                    </button>
                 @else
                 @endif
             @endauth
@@ -32,4 +48,5 @@
         </div>        
         </div>
     </div>
+    @include('main.modal')
 @endforeach
